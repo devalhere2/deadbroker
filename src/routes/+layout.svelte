f342961361754eb6bc7b1afdaf87f2f850aa194b
deval <deval2299@gmail.com>
2024-11-18 23:36:04 +0530

    svelte shadcn setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.css';
+    import { Input } from "$lib/components/ui/input";
+
+	let { children } = $props();
+</script>
+
+<nav class="flex h-12">
+	<div class="grow flex ">
+		<div class="site-name">deadbroker</div>
+	</div>
+	<div class="search-bar w-80">
+		<Input type="text" placeholder="Search Locality ..." class="h-8"/>
+	</div>
+	<div class="profile">Profile</div>
+</nav>
+
+
+{@render children()}
